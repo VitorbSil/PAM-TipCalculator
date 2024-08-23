2a1b73c63247a1c059f3e8659744c7b36fc1e6e1
Vitor Barbosa Silva <Vitorbarbosa062@gmail.com>
2024-08-23 20:51:51 -0300

    Primeiro commit

---
+++ TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SetLowTip_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void SetHighTip_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void RoundUpButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void RoundDownButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
