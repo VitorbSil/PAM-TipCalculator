581ccbbe6d6fca26fdb36978b3870788a02c86cd
Vitor Barbosa Silva <Vitorbarbosa062@gmail.com>
2024-08-23 22:43:17 -0300

    Adicionada a lógica do cálculo da gorjeta

--- TipCalculator/MainPage.xaml.cs
+++ TipCalculator/MainPage.xaml.cs
@@ -10,12 +10,14 @@
 
         private void SetLowTip_Clicked(System.Object sender, System.EventArgs e)
         {
-
+            TipSlider.Value = 15;
+            TipPercentLabel.Text = TipSlider.Value.ToString() + "%";
         }
 
         private void SetHighTip_Clicked(System.Object sender, System.EventArgs e)
         {
-
+            TipSlider.Value = 20;
+            TipPercentLabel.Text = TipSlider.Value.ToString() + "%";
         }
 
         private void RoundUpButton_Clicked(System.Object sender, System.EventArgs e)
@@ -26,6 +28,21 @@
         private void RoundDownButton_Clicked(System.Object sender, System.EventArgs e)
         {
 
+        }
+
+        private void TipSlider_ValueChanged(System.Object sender, Microsoft.Maui.Controls.ValueChangedEventArgs e)
+        {
+            TipPercentLabel.Text = TipSlider.Value.ToString("#") + "%";
+        }
+
+        private void AmountEntry_TextChanged(System.Object sender, Microsoft.Maui.Controls.TextChangedEventArgs e)
+        {
+            double Valor = Convert.ToDouble(AmountEntry.Text);
+            double ValorDaGorjeta = Valor * (TipSlider.Value / 100);
+            double ValorTotal = ValorDaGorjeta + Valor;
+
+            TotalLabel.Text = ValorTotal.ToString("C");
+            TipLabel.Text = ValorDaGorjeta.ToString("C");
         }
     }
 }
